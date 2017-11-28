820b2ea8baf419a022b183867269bbc1deb00f36
sriramveeraghanta <veeraghanta.sriram@gmail.com>
2017-11-28 15:33:42 +0530

    Settings updated

--- project_name/wsgi.py
+++ project_name/wsgi.py
@@ -1,14 +1,10 @@
-"""
-WSGI config for {{ project_name }} project.
-It exposes the WSGI callable as a module-level variable named ``application``.
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "{{project_name}}.settings.production")
+from whitenoise.django import DjangoWhiteNoise # noqa
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)

c203c1b602749f995b97a57eef2c5be8a26fbc02
sriramveeraghanta <veeraghanta.sriram@gmail.com>
2020-11-11 18:32:22 -0500

    cookiecutter update

--- project_name/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name="base.html")),
-]
